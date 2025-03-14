181dbcb172e743657ddb4c2823bcc1e9d0d24278
Mohd Aman Naim Patel <ap5277478@gmail.com>
2025-03-15 00:37:54 +0530

    Update DB.connect.js

--- connection_config/DB.connect.js
+++ connection_config/DB.connect.js
@@ -1,11 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
-console.log("process.env.PGUSER", process.env.PGUSER);
-console.log("process.env.PGPASSWORD", process.env.PGPASSWORD);
-console.log("process.env.PGDATABASE", process.env.PGDATABASE);
-console.log("process.env.PGHOST", process.env.PGHOST);
-console.log("process.env.PGPORT", process.env.PGPORT);
 const sequelize = new Sequelize(
   process.env.PGDATABASE,
   process.env.PGUSER,
